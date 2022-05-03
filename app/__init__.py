b6063f1ef4932381c4cfa41978186710b0db2b69
Machuka Joy <machukajoy@gmail.com>
2022-05-03 20:40:12 +0300

    ft(config): add config initializtion and create app function

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from config import config_options
+
+
+#app variable name
+app = Flask(__name__)
+
+def create_app(config_name):
+
+    app.config.from_object(config_options[config_name])
+
+     #register blueprint
+    from main import main as main_blueprint 
+    app.register_blueprint(main_blueprint)
+
+    return app 
