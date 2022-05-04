69aba0455230e4f7bc231034e20ea414297f928f
Machuka Joy <machukajoy@gmail.com>
2022-05-04 09:09:04 +0300

    ft(date function): add date function to process dates

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,27 @@
 from flask import Flask
 from config import config_options
 
+from datetime import datetime
 
-#app variable name
-app = Flask(__name__)
+def format_datetime(value, format="%d-%m-%Y"):
+    if value is None:
+        return ""
+    return datetime.strptime(value,"%Y-%m-%d").strftime(format)
+
+#configured Jinja2 environment with user defined
+
 
 def create_app(config_name):
+    app = Flask(__name__)
 
     app.config.from_object(config_options[config_name])
 
      #register blueprint
-    from main import main as main_blueprint 
+    from .main import main as main_blueprint 
     app.register_blueprint(main_blueprint)
+    app.jinja_env.filters['date_format']=format_datetime
+
+    from .requests import configure_request
+    configure_request(app)
 
     return app 
